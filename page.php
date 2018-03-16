13562a85832a277c265d294292bdf55e2c2d9336
https://github.com/bruutenbraaf/bruutenbraf.git <info@bruutenbraaf.nl>
2018-03-16 23:40:31 +0100

    Little changes

--- page.php
+++ page.php
@@ -5,8 +5,7 @@ get_header(); ?>
 	<?php if(is_front_page()): ?>
 		<div class="home_carousel">	
 			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-			  <div class="carousel-inner">
-				  
+			  <div class="carousel-inner">				  
 			    <div class="carousel-item" style="background:url('http://www.massagepraktijkfysiek.nl/res/img/therapy-massage.jpg');">
 				    <div class="container">
 					    <div class="row">
@@ -16,12 +15,7 @@ get_header(); ?>
 						    </div>
 					    </div>
 				    </div>
-			    </div>
-	<!--		    
-			    <div class="carousel-item" style="background:url('https://www.sportmassagehro.nl/wp-content/uploads/2015/03/iStock-157962237.jpg');">
-			    </div>
-			    -->
-			    
+			    </div>			    
 			  </div>
 			  <a href="#content"><div class="down">
 				  <svg viewBox="0 0 31.5 31.5" style="enable-background:new 0 0 31.5 31.5;" xml:space="preserve">
@@ -36,17 +30,14 @@ get_header(); ?>
 						<path class="st0" d="M21.2,5c-0.4-0.4-1.1-0.4-1.6,0c-0.4,0.4-0.4,1.1,0,1.6l8,8H1.1c-0.6,0-1.1,0.5-1.1,1.1c0,0.6,0.5,1.1,1.1,1.1
 							h26.6l-8,8c-0.4,0.4-0.4,1.2,0,1.6c0.4,0.4,1.2,0.4,1.6,0l10-10c0.4-0.4,0.4-1.1,0-1.6L21.2,5z"/>
 						</svg>
-
 				  </a>
 				  <div class="seperator">
 				  </div>
 				  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
 				    <svg viewBox="0 0 31.5 31.5" style="enable-background:new 0 0 31.5 31.5;" xml:space="preserve">
-<path class="st0" d="M21.2,5c-0.4-0.4-1.1-0.4-1.6,0c-0.4,0.4-0.4,1.1,0,1.6l8,8H1.1c-0.6,0-1.1,0.5-1.1,1.1c0,0.6,0.5,1.1,1.1,1.1
-	h26.6l-8,8c-0.4,0.4-0.4,1.2,0,1.6c0.4,0.4,1.2,0.4,1.6,0l10-10c0.4-0.4,0.4-1.1,0-1.6L21.2,5z"/>
-</svg>
-
-
+						<path class="st0" d="M21.2,5c-0.4-0.4-1.1-0.4-1.6,0c-0.4,0.4-0.4,1.1,0,1.6l8,8H1.1c-0.6,0-1.1,0.5-1.1,1.1c0,0.6,0.5,1.1,1.1,1.1
+							h26.6l-8,8c-0.4,0.4-0.4,1.2,0,1.6c0.4,0.4,1.2,0.4,1.6,0l10-10c0.4-0.4,0.4-1.1,0-1.6L21.2,5z"/>
+						</svg>
 				  </a>
 			  </div>
 			</div>	
